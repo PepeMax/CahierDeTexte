cd2f7a0d4e43a3430348235bdef1321fcc680f7f
Smozeee <hugoferreira107@hotmail.com>
2019-11-27 15:04:30 +0100

    maj du 17/11

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import { AuthService } from 'src/app/services/auth.service';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +19,8 @@ export class HomePage implements OnInit {
     public trans: TranslateService,
     public navCtrl: NavController,
     public storage: Storage,
+    public auth : AuthService,
+    public alertController: AlertController
 
   ) { }
 
@@ -26,5 +31,27 @@ export class HomePage implements OnInit {
   goSettings() {
     this.navCtrl.navigateForward("/settings")
   }
-
+  
+  async disconnect() {
+    const alert = await this.alertController.create({
+      header: this.trans.instant('HOME.DISCONNECT'),
+      message: this.trans.instant('HOME.CONFIRM_DISCONNECT'),
+      buttons: [
+        {
+          text: this.trans.instant('COMMON.CANCEL'),
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+          }
+        }, {
+          text: this.trans.instant('COMMON.VALIDATE'),
+          handler: () => {
+            this.navCtrl.navigateRoot("/login");
+            this.storage.clear();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
