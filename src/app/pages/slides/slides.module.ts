ba633779478a9c844682be91a2463c6817c57cdd
Smozeee <hugoferreira107@hotmail.com>
2019-11-28 10:16:19 +0100

    28/11/19

--- src/app/pages/slides/slides.module.ts
+++ src/app/pages/slides/slides.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { SlidesPage } from './slides.page';
 import { ComponentsModule } from 'src/app/components/components.module';
 
@@ -20,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslateModule.forChild(),
     ComponentsModule,
     RouterModule.forChild(routes)
   ],
