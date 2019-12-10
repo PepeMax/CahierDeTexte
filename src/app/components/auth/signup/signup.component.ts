83ee95935e5f71a163046d7ffe4f10662ebd290d
mdenost <mdenost@inflexsys.com>
2019-12-10 16:33:13 +0100

    Create user in prof ans student

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import { NavController, LoadingController, ModalController, AlertController } from '@ionic/angular';
+import { NavController, LoadingController, ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { CheckodeComponent } from 'src/app/components/auth/checkode/checkode.component';
+import { Storage } from '@ionic/storage';
+import { HandleErrorService } from 'src/app/services/handle-error.service';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-signup',
@@ -18,15 +21,15 @@ export class SignupComponent implements OnInit {
   private codeChecked: boolean = false;
   private nbChance: number = 3;
 
-
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private navCtrl: NavController,
     private loadingCtrl: LoadingController,
     private modalCtrl: ModalController,
-    private alertController: AlertController,
     public trans: TranslateService,
+    private storage: Storage,
+    private handleErr: HandleErrorService,
   ) { }
 
   ngOnInit() {
@@ -42,17 +45,21 @@ export class SignupComponent implements OnInit {
   }
 
   async onSubmit() {
+    const db = firebase.firestore();
 
     const email = this.signupForm.get('email').value;
     const password = this.signupForm.get('password').value;
     const status = this.signupForm.get('status').value;
+
+    this.storage.set('email', email);
+    this.storage.set('password', password);
 
     const loading = await this.loadingCtrl.create({
       backdropDismiss: false,
       spinner: "crescent",
     });
 
-    if (status === "prof") {
+    if (status === "professor") {
       const modalCheckCode = await this.modalCtrl.create({
         component: CheckodeComponent,
         id: "modalCheckCode",
@@ -69,20 +76,24 @@ export class SignupComponent implements OnInit {
       modalCheckCode.present();
     } else if (status === "student") {
       loading.present();
-      this.authService.createNewUser(email, password)
-        .then(() => {
-          console.log("bien crée")
-          this.authService.signInUser(email, password)
-          .then(() => {
-            this.navCtrl.navigateRoot("nav/home")
-            loading.dismiss();
-          })
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(cred => {
+          return db.collection('users').doc(cred.user.uid).set({
+            email: email,
+            password: password,
+            name: name
+          }).then(() => {
+            this.authService.signInUser(email, password)
+              .then(() => {
+                this.navCtrl.navigateRoot("nav/home");
+                loading.dismiss();
+              });
+          });
+        }).catch((error) => {
+          this.errorMessage = this.handleErr.handleError(error);
+          loading.dismiss();
         });
-      (error) => {
-        console.log("érreur")
-        this.errorMessage = error;
-        loading.dismiss();
-      };
+
     }
 
   }
